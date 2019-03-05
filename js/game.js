861657c2344e93e355f6ddcd9d62938e4d7a79d4
Мария Филиппова <marykaweb@gmail.com>
2019-03-05 21:27:32 +0300

    first commit

---
+++ js/game.js
@@ -0,0 +1,55 @@
+var game;
+window.onload = function() {
+
+	var gameConfig = {
+		width: 1480,
+		height: 1640,
+		backgroundColor: 0xff0000,
+		scene: [bootGame, playGame]
+	}
+		
+	game = new Phaser.Game(gameConfig);
+	window.focus();
+	resizeGame();
+	window.addEventListener("resize", resizeGame);
+}
+
+class playGame extends Phaser.Scene{
+	constructor(){
+		super("PlayGame");
+	}
+	create(){
+		console.log("this is my awesome game");
+	}
+}
+
+class bootGame extends Phaser.Scene{
+	constructor(){
+		super("BootGame");
+	}
+	create(){
+		console.log("game is booting...");
+		this.scene.start("PlayGame");
+	}
+}
+
+
+/**
+*	Functions
+**/
+
+function resizeGame() {
+	var canvas = document.querySelector("canvas");
+	var windowWidth = window.innerWidth;
+	var windowHeight = window.innerHeight;
+	var windowRatio = windowWidth / windowHeight;
+	var gameRatio = game.config.width / game.config.height;
+	if(windowRatio < gameRatio){
+		canvas.style.width = windowWidth + "px";
+		canvas.style.height = (windowWidth / gameRatio) + "px";
+	}
+	else{
+		canvas.style.width = (windowHeight * gameRatio) + "px";
+		canvas.style.height = windowHeight + "px";
+	}
+}
